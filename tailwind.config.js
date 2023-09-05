70e730947ce0614d7f454635b7081236138d333f
Edgar <jorge260590@hotmail.com>
2023-09-04 19:45:51 -0500

    feat(First commit): Insert new proyect ready to code

---
+++ tailwind.config.js
@@ -0,0 +1,34 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,js}", "./index.html"],
+  theme: {
+    extend: {
+      colors: {
+        lightRed: 'hsl(0, 100%, 67%)',
+        orangeYellow: 'hsl(39, 100%, 56%)',
+        greenTeal: 'hsl(166, 100%, 37%)',
+        cobaltBlue: 'hsl(234, 85%, 45%)',
+        lightSlateBlue: 'hsl(252, 100%, 67%)',
+        lightRoyalBlue: 'hsl(241, 81%, 54%)',
+        white: 'hsl(0, 0%, 100%)',
+        paleBlue: 'hsl(221, 100%, 96%)',
+        lightLavender: 'hsl(241, 100%, 89%)',
+        darkGrayBlue: 'hsl(224, 30%, 27%)',
+      },
+      fontSize:{
+        textBase: '1.125rem'
+      },
+      fontFamily: {
+        hanken: ['Hanken Grotesk','sans']
+      },
+      fontWeight:{
+        500: '500',
+        600: '600',
+        700: '700'
+      
+    },
+    plugins: [],
+    },
+  }
+}
+
