4a5f1f4a5963ea573bc530347c4ff90a67edeb78
Edgar <jorge260590@hotmail.com>
2023-09-08 17:12:44 -0500

    style(colors) Add all colors and icon images, some gradient and color opacity

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,13 +5,18 @@ module.exports = {
     extend: {
       colors: {
         lightRed: 'hsl(0, 100%, 67%)',
+        lightRedBg: 'hsla(0, 100%, 67%, 0.06)',
         orangeYellow: 'hsl(39, 100%, 56%)',
+        orangeYellowBg: 'hsla(39, 100%, 56%, 0.06)',
         greenTeal: 'hsl(166, 100%, 37%)',
+        greenTealBg: 'hsla(166, 100%, 37%, 0.06)',
         cobaltBlue: 'hsl(234, 85%, 45%)',
+        cobaltBlueBg: 'hsla(234, 85%, 45%, 0.06)',
         white: 'hsl(0, 0%, 100%)',
         paleBlue: 'hsl(221, 100%, 96%)',
         lightLavender: 'hsl(241, 100%, 89%)',
         darkGrayBlue: 'hsl(224, 30%, 27%)',
+        lightDarkGrayBlue: 'hsla(224, 30%, 27%, 0.5)',
       },
       fontSize:{
         textBase: '1.125rem'
